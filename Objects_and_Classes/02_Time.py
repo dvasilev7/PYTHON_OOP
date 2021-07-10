b6234dde5e9988835b3a92a9e3f6f6dbb6ae3cfd
dvasilev7 <Denio-103734-github>
2021-07-10 21:54:26 +0300

    adding the code

---
+++ Objects_and_Classes/02_Time.py
@@ -0,0 +1,42 @@
+class Time:
+    max_hours = 23
+    max_minutes = 59
+    max_seconds = 59
+
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def set_time(self, hours, minutes, seconds):
+        if 0 <= minutes <= 59 and 0 <= seconds <= 59 and 0 <= hours <= 23:
+            self.hours = hours
+            self.minutes = minutes
+            self.seconds = seconds
+        return self.get_time()
+
+    def get_time(self):
+        return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
+
+    def next_second(self):
+        if self.seconds == Time.max_seconds:
+            self.seconds = 0
+            if self.minutes == Time.max_minutes:
+                self.minutes = 0
+                if self.hours == Time.max_hours:
+                    self.hours = 0
+                else:
+                    self.hours += 1
+            else:
+                self.minutes += 1
+        else:
+            self.seconds += 1
+        return self.get_time()
+
+
+time = Time(9, 30, 59)
+print(time.next_second())
+time = Time(10, 59, 59)
+print(time.next_second())
+time = Time(23, 59, 59)
+print(time.next_second())
