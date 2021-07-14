02fb73b8f40bf1ef571efeeda4ab88adc63ecc21
dvasilev7 <Denio-103734-github>
2021-07-14 16:03:26 +0300

    inheritance exercise

---
+++ Inheritance/ANIMAL/gorilla.py
@@ -0,0 +1,6 @@
+from ANIMAL.mammal import Mammal
+
+
+class Gorilla(Mammal):
+    def __init__(self, name):
+        super().__init__(name)
---
+++ Inheritance/ANIMAL/mammal.py
@@ -0,0 +1,6 @@
+from ANIMAL.animal import Animal
+
+
+class Mammal(Animal):
+    def __init__(self, name):
+        super().__init__(name)
